6fb9e10f39c4a26d104b66efeb09db393c81b95a
Coolp-GitHub <75265429+Coolp-GitHub@users.noreply.github.com>
2021-06-01 21:25:40 -0400

    Home

--- Interactions.js
+++ Interactions.js
@@ -1,7 +1,12 @@
 
 
-document.getElementById('box').addEventListener('click', function()
+document.getElementById('About').addEventListener('click', function()
 {
     location.href = "About.html"
+
+});
+document.getElementById('Home').addEventListener('click', function()
+{
+    location.href = "Index.html"
 
 });
